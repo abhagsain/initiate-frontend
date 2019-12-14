12fb651f11ee02d4b79e7b99c6753ad124f35acd
Anurag Bhagsain <abhagsain@gmail.com>
2019-12-15 03:20:03 +0530

    add apollo client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
 import "./App.css";
 import "./index.css";
-function App() {
-  return (
-    <div className="App">
-      <h2 className="text-2xl">Initiate React</h2>
-    </div>
-  );
+import { ApolloProvider, Query } from "react-apollo";
+import { ApolloClient, InMemoryCache, gql, HttpLink } from "apollo-boost";
+const link = new HttpLink({
+  uri: "http://192.168.99.100:4488"
+});
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+const getHomePagePosts = gql`
+  query getHomepagePosts {
+    posts {
+      title
+      body
+      author {
+        email
+      }
+    }
+  }
+`;
+class App extends React.Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Query query={getHomePagePosts}>
+          {({ data, loading, error }) => {
+            console.log("TCL: App -> render -> data", data);
+            if (loading) return loading && <h2>Loading</h2>;
+            return (
+              <div className="App">
+                {data.posts.map(el => (
+                  <h2>{el.title}</h2>
+                ))}
+              </div>
+            );
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
